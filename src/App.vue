d25fa4859179ed8bed772b884edd980ec8a574b4
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-06-19 23:25:14 +0200

    [DOCS] Esercizio completato

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<!-- comprende la parte logica (javascript) -->
+<script>
+// importa la componente AppTitle.vue
+import AppTitle from './components/AppTitle.vue';
+export default {
+  // oggetto che esporta la componente 
+  components:{
+    AppTitle
 
+  },
+  
+}
+</script>
+<!-- comprende la parte grafica (html) -->
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <!-- riprende la componente figlio che sarà il file AppTitle.vue -->
+    <AppTitle/>
+    
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<!-- comprende lo stile della pagina (css) -->
+<style>
+  
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  
+}
+</script>
+<template>
+  <div>
+    <!-- titolo che verra importato nella componente padre che è rappresentato dal file App.vue -->
+    <h1>La mia prima app con Vite!</h1> 
+  </div>
+</template>
+
+<style>
+  
+</style>
